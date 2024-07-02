c299dce350136937d48782e56856aab02b8d7eee
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2024-07-02 18:25:53 +0700

    adjust width of image

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def main():
     class_btn = st.button("Classify")
     if file_uploaded is not None:    
         image = Image.open(file_uploaded)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', use_column_width=True, width=300)
         
     if class_btn:
         if file_uploaded is None:
