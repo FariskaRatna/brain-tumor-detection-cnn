d5a24e92a3acbe8623f262172c12e72f8fd68da5
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2024-07-02 11:31:10 +0700

    remove pil

--- app.py
+++ app.py
@@ -20,26 +20,6 @@ st.title('Brain Tumor Classifier')
 st.markdown("Welcome to this simple web application that classifies brain tumors. The tumors are classified into four different classes namely: Glioma Tumor, No Tumor, Meningioma Tumor, and Pituitary Tumor.")
 
 
-def main():
-    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
-    class_btn = st.button("Classify")
-    if file_uploaded is not None:    
-        image = Image.open(file_uploaded)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        
-    if class_btn:
-        if file_uploaded is None:
-            st.write("Invalid command, please upload an image")
-        else:
-            with st.spinner('Model working....'):
-                plt.imshow(image)
-                plt.axis("off")
-                predictions = predict(image)
-                time.sleep(1)
-                st.success('Classified')
-                st.write(predictions)
-                st.pyplot(fig)
-
 def download_model(url, filename):
     response = requests.get(url)
     with open(filename, 'wb') as f:
@@ -55,7 +35,7 @@ def predict(image_path):
     IMAGE_SHAPE=(512,512,3)
     model = cnn_model()
     model.load_weights(classifier_model)
-    image = Image.open(image_path)
+    # image = Image.open(image_path)
     test_image = image.resize((IMAGE_SHAPE[0], IMAGE_SHAPE[1]))
     test_image = np.array(test_image)
     test_image = np.expand_dims(test_image, axis=0)
@@ -77,6 +57,26 @@ def predict(image_path):
     
     results = f"{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence."
     return results
+
+def main():
+    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
+    class_btn = st.button("Classify")
+    if file_uploaded is not None:    
+        image = Image.open(file_uploaded)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+    if class_btn:
+        if file_uploaded is None:
+            st.write("Invalid command, please upload an image")
+        else:
+            with st.spinner('Model working....'):
+                plt.imshow(image)
+                plt.axis("off")
+                predictions = predict(image)
+                time.sleep(1)
+                st.success('Classified')
+                st.write(predictions)
+                st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
